6abcbe68c24a0e7f2022676ea4a682daeae78787
Ian Hetterich <ihetterich@gmail.com>
2020-12-17 16:13:21 -0700

    Model up and running

--- src/eda.py
+++ src/eda.py
@@ -7,16 +7,17 @@ import pandas as pd
 class DataPipeline:
     def __init__(self, df):
         self.df = df
-        self.fraud = self.df[self.df['fraud'] == 1]
-        self.not_fraud = self.df[self.df['fraud'] == 0]
+        
 
     def add_fraud(self):
         """
         This function adds the 'fraud' column initially assigned to False. If the column 'acct_type
         has fraud contained anywhere in it, a wild card should pick that up and assign a True value to that row.
         """
-        self.df['fraud'] = False
-        self.df['fraud'][self.df['acct_type'].str.contains('fraud(?!$)')] = True
+        self.df['fraud'] = 0
+        self.df['fraud'][self.df['acct_type'].str.contains('fraud(?!$)')] = 1
+        # self.fraud = self.df[self.df['fraud'] == 1]
+        # self.not_fraud = self.df[self.df['fraud'] == 0]
 
     def count_fraud(self):
         print(f'Total amount of fraudulent cases:', self.df['fraud'].sum())  # prints out total sum of fraud cases
@@ -38,8 +39,10 @@ class DataPipeline:
         self.df['user_age'] = self.df['user_age'].astype(float)
         self.df['has_address'] = self.df['venue_address'].str.len() > 0  # Creates new column of binary value 0 or 1
         self.df['num_previous_payouts'] = self.df['previous_payouts'].apply(lambda x: len(x))
+        self.df['num_ticket_types'] = self.df['ticket_types'].apply(lambda x: len(x))
         self.df['e_dom1'] = self.df['email_domain'].apply(lambda x: x.split(".")[0])
         self.df['e_dom2'] = self.df['email_domain'].apply(lambda x: ''.join(x.split(".")[1:]))
+        self.df['listed'] = self.df['listed'].replace({'y': 1, 'n': 0})
 
     def test_script_examples(self):
         fraud_examples = self.fraud.sample(2)
--- src/model.py
+++ src/model.py
@@ -3,8 +3,11 @@ import pandas as pd
 import pickle
 
 from bs4 import BeautifulSoup
+from eda import DataPipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+pd.set_option('display.max_columns', None)
 
 class MyModel():
 
@@ -22,36 +25,44 @@ class MyModel():
 
 def get_data(filepath):
     df = pd.read_json(filepath)
+    pipe = DataPipeline(df)
+    pipe.add_fraud()
+    pipe.drop_leaky()
+    pipe.clean()
 
-    df['fraud'] = False
-    df['fraud'][df['acct_type'].str.contains('fraud(?!$)')] = True
-    y = df['fraud']
+    y = pipe.df['fraud']
 
     text = df['description'].apply(lambda text: BeautifulSoup(text, 'html.parser').get_text())
-    vecto = TfidfVectorizer(stop_words='english', max_features=50)
-    vecto.fit(text)
-    text_vect = vecto.transform(text)
+    vecto = TfidfVectorizer(stop_words='english', max_features=5)
+    text_vect = vecto.fit_transform(text)
     text_df = pd.DataFrame(text_vect.todense())
+    # print(text_df.head())
 
-    df['num_previous_payouts'] = df['previous_payouts'].apply(lambda x: len(x))
-    df['e_dom1'] = df['email_domain'].apply(lambda x: x.split(".")[0])
-    df['e_dom2'] =  df['email_domain'].apply(lambda x: ''.join(x.split(".")[1:]))  
-    df['has_address'] = df['venue_address'].str.len() > 0 
+    names = df['name']
+    name_vecto = TfidfVectorizer(stop_words='english', max_features=5)
+    name_vect = name_vecto.fit_transform(names)
+    name_df = pd.DataFrame(name_vect.todense())
 
-    variable = ['country', 'e_dom1', 'e_dom2']
-    df_one_hot = pd.get_dummies(df,columns=variable,drop_first=True)
+    org_desc = df['org_desc'].apply(lambda text: BeautifulSoup(text, 'html.parser').get_text())
+    org_vecto = TfidfVectorizer(stop_words='english', max_features=5)
+    org_vect = org_vecto.fit_transform(org_desc)
+    org_df = pd.DataFrame(org_vect.todense())
 
-    # X = df[['body_length', 'channels', 'currency', 'delivery_method',
-    #     'event_created', 'event_end', 'event_published',
-    #     'event_start', 'fb_published', 'has_analytics', 'has_header', 
-    #     'has_logo', 'listed', 'name', 'name_length', 'num_order',       
-    #     'object_id', 'org_desc', 'org_facebook', 'org_name', 'org_twitter',       
-    #     'payee_name', 'num_previous_payouts', 'show_map', 'ticket_types',       
-    #     'user_age', 'user_created', 'user_type', 'has_address', 
-    #     'venue_country', 'venue_state', 'fraud']]
-    X = df[['gts']]
-    pd.concat([X, text_df, df_one_hot], axis=1)
-
+    variables = ['country', 'e_dom1', 'e_dom2', 'currency', 'venue_state', 'venue_country']
+    
+    X = pipe.df[['body_length', 'channels', 'delivery_method',
+        'event_created', 'event_end', 'event_published',
+        'event_start', 'fb_published', 'has_analytics', 'has_header', 
+        'has_logo', 'listed', 'name_length', 'num_order',       
+        'object_id', 'org_facebook', 'org_twitter',       
+        'num_ticket_types', 'num_previous_payouts', 'show_map',       
+        'user_age', 'user_created', 'user_type', 'has_address', 
+        'country', 'e_dom1', 'e_dom2', 'currency', 'venue_state', 'venue_country']]
+    
+    df_one_hot = pd.get_dummies(X,columns=variables,drop_first=True)
+    
+    X = pd.concat([text_df, name_df, org_df, df_one_hot], axis=1)
+    # print(X.head(1))
     return X, y
 
 if __name__ == '__main__':
